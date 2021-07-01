2daa3d4eaec0e9fd8edf8dffc6fe87b7f4bda788
DongGeon2 <rlagjqm1@naver.com>
2021-07-01 09:50:53 +0900

    이건받으세요

--- Final-Project/src/test/java/org/kosta/myproject/FinalProjectApplicationTests.java
+++ Final-Project/src/test/java/org/kosta/myproject/FinalProjectApplicationTests.java
@@ -1,13 +1,27 @@
 package org.kosta.myproject;
 
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+
 import org.junit.jupiter.api.Test;
+import org.kosta.myproject.model.service.PostService;
+import org.kosta.myproject.model.vo.PagingBean;
+import org.kosta.myproject.model.vo.PostVO;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FinalProjectApplicationTests {
-
+@Resource
+private PostService ps;
 	@Test
 	void contextLoads() {
+		ArrayList<PostVO> list=ps.getList(new PagingBean(3));
+		PagingBean pb = new PagingBean(3);
+		System.out.println(pb);
+		pb = new PagingBean(3,1);
+		System.out.println(pb);
+		System.out.println(list);
 	}
 	/* tettstets */
 }
