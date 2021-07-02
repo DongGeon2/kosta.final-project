42dd46672f2483ad77ee4846be5408f3fd870252
DongGeon2 <rlagjqm1@naver.com>
2021-07-02 17:01:50 +0900

    예약하기 v1 동건

--- Final-Project/src/main/java/org/kosta/myproject/model/mapper/ReservationMapper.java
+++ Final-Project/src/main/java/org/kosta/myproject/model/mapper/ReservationMapper.java
@@ -1,8 +1,11 @@
 package org.kosta.myproject.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.kosta.myproject.model.vo.ReservationVO;
 
 @Mapper
 public interface ReservationMapper {
+
+	void registerReservation(ReservationVO resVO);
 
 }
